a7bffcc2bbe3c933d9ddbd30dcbe155215199298
vendidero <info@vendidero.de>
2022-04-29 12:58:43 +0200

    Fake wc admin existence.

--- woocommerce-disable-wc-admin.php
+++ woocommerce-disable-wc-admin.php
@@ -18,123 +18,21 @@ if ( ! defined( 'ABSPATH' ) ) {
 	exit; // Exit if accessed directly
 }
 
-add_filter( 'woocommerce_admin_disabled', '__return_true', 500 );
-
-add_filter( 'woocommerce_data_stores', function( $data_stores ) {
-	include_once( __DIR__ . '/data-store-admin-note-replacement.php' );
-
-	$data_stores['admin-note'] = new DataStoreAdminNoteReplacement();
-
-	return $data_stores;
-}, 1000 );
-
 add_action( 'plugins_loaded', function() {
-	$loader   = 'Automattic\WooCommerce\Admin\Loader';
-	$features = 'Automattic\WooCommerce\Admin\Features\Features';
-
-	if ( class_exists( $features ) ) {
-		$loader = $features;
+	/**
+	 * Fake existence of WC Admin
+	 */
+	if ( ! defined( 'WC_ADMIN_VERSION_NUMBER' ) ) {
+		define( 'WC_ADMIN_VERSION_NUMBER', '1.0' );
 	}
 
-	if ( class_exists( $loader ) ) {
-		// Handler for WooCommerce and WooCommerce Admin plugin activation.
-		remove_action( 'woocommerce_activated_plugin', array( $loader, 'activated_plugin' ) );
-		remove_action( 'activated_plugin', array( $loader, 'activated_plugin' ) );
+	if ( ! defined( 'WC_ADMIN_PLUGIN_FILE' ) ) {
+		define( 'WC_ADMIN_PLUGIN_FILE', 'woocommerce-admin/woocommerce-admin.php' );
 	}
 
-	$composer_package = 'Automattic\WooCommerce\Admin\Composer\Package';
-
-	if ( class_exists( $composer_package ) ) {
-		remove_action( 'woocommerce_init', array( $composer_package, 'check_outdated_wca_plugin' ) );
+	if ( ! defined( 'WC_ADMIN_APP' ) ) {
+		define( 'WC_ADMIN_APP', 'wc-admin-app-none' );
 	}
+}, 5 );
 
-	if ( ! function_exists( 'wc_admin_is_connected_page' ) ) {
-		function wc_admin_is_connected_page() {
-			return false;
-		}
-	}
-
-	if ( ! function_exists( 'wc_admin_connect_page' ) ) {
-		function wc_admin_connect_page( $options ) {
-			return false;
-		}
-	}
-
-	if ( ! function_exists( 'wc_admin_register_page' ) ) {
-		function wc_admin_register_page( $options ) {
-			return false;
-		}
-	}
-
-	if ( ! function_exists( 'wc_admin_is_registered_page' ) ) {
-		function wc_admin_is_registered_page() {
-			return false;
-		}
-	}
-
-	if ( ! function_exists( 'wc_admin_get_breadcrumbs' ) ) {
-		function wc_admin_get_breadcrumbs() {
-			return array();
-		}
-	}
-}, 1000 );
-
-add_filter( 'woocommerce_admin_features', function() {
-	return array();
-}, 1000 );
-
-add_action( 'admin_init', function() {
-	add_filter( 'woocommerce_admin_features', function() {
-		return array();
-	}, 100 );
-
-	$classname = 'Automattic\WooCommerce\Admin\Loader';
-	$features  = 'Automattic\WooCommerce\Admin\Features\Features';
-	$assets    = 'Automattic\WooCommerce\Internal\Admin\WCAdminAssets';
-
-	if ( class_exists( $assets ) ) {
-		$instance  = $assets::get_instance();
-
-		remove_action( 'admin_enqueue_scripts', array( $instance, 'register_scripts' ) );
-
-		remove_action( 'admin_enqueue_scripts', array( $instance, 'inject_wc_settings_dependencies' ), 14 );
-		remove_action( 'admin_enqueue_scripts', array( $instance, 'enqueue_assets' ), 15 );
-	}
-
-	if ( class_exists( $features ) ) {
-		$classname = $features;
-	}
-
-	if ( class_exists( $classname ) ) {
-		$instance  = $classname::get_instance();
-
-		remove_action( 'admin_enqueue_scripts', array( $classname, 'register_scripts' ) );
-		remove_action( 'admin_enqueue_scripts', array( $classname, 'inject_wc_settings_dependencies' ), 14 );
-		remove_action( 'admin_enqueue_scripts', array( $instance, 'load_scripts' ), 15 );
-		// Old settings injection.
-		remove_filter( 'woocommerce_components_settings', array( $classname, 'add_component_settings' ) );
-		// New settings injection.
-		remove_filter( 'woocommerce_shared_settings', array( $classname, 'add_component_settings' ) );
-		remove_filter( 'admin_body_class', array( $classname, 'add_admin_body_classes' ) );
-		remove_action( 'admin_menu', array( $classname, 'register_page_handler' ) );
-		remove_action( 'admin_menu', array( $classname, 'register_store_details_page' ) );
-		remove_filter( 'admin_title', array( $classname, 'update_admin_title' ) );
-		remove_action( 'rest_api_init', array( $classname, 'register_user_data' ) );
-		remove_action( 'in_admin_header', array( $classname, 'embed_page_header' ) );
-		remove_filter( 'woocommerce_settings_groups', array( $classname, 'add_settings_group' ) );
-		remove_filter( 'woocommerce_settings-wc_admin', array( $classname, 'add_settings' ) );
-		remove_action( 'admin_head', array( $classname, 'remove_notices' ) );
-		remove_action( 'admin_head', array( $classname, 'smart_app_banner' ) );
-	}
-
-	$classname = 'Automattic\WooCommerce\Admin\Features\Features';
-
-	if ( class_exists( $classname ) ) {
-		remove_filter( 'woocommerce_get_sections_advanced', array( $classname, 'add_features_section' ) );
-		remove_filter( 'woocommerce_get_settings_advanced', array( $classname, 'add_features_settings' ), 10 );
-		remove_action( 'admin_enqueue_scripts', array( $classname, 'maybe_load_beta_features_modal' ) );
-		remove_action( 'admin_enqueue_scripts', array( $classname, 'load_scripts' ), 15 );
-		remove_filter( 'admin_body_class', array( $classname, 'add_admin_body_classes' ) );
-		remove_filter( 'update_option_woocommerce_allow_tracking', array( $classname, 'maybe_disable_features' ), 10 );
-	}
-}, 100 );
+add_filter( 'woocommerce_admin_disabled', '__return_true', 500 );
